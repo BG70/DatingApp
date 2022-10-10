2cfccfad1ae04ea15d60ba44613c67ffbeb817f4
Benjamin Guller <7753373@gmail.com>
2022-10-10 10:24:31 +0300

    #End of 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context){
+            _context = context;
+        }
+        
+        // api/users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = _context.Users.ToListAsync();
+            return await users;
+        }
+        // רצינו שליפה אסינכרוני
+
+        //public ActionResult<IEnumerable<AppUser>> GetUsers()
+        //{
+        //    return _context.Users.ToList();
+        //}
+
+
+
+        // api/users/3
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = _context.Users.FindAsync(id);
+            return await user;
+        }   
+
+        //public ActionResult<AppUser> GetUser(int id)
+        //{
+        //    return _context.Users.Find(id);
+        //}              
+    }
+}
